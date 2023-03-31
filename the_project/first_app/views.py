bdfc61440d9af6100d2db89b5fdb495a8f8904fa
SimaElisabeta <sima.elisabeta@gmail.com>
2023-03-31 16:41:29 +0300

    rendering index page with records stored in my view context

--- the_project/first_app/views.py
+++ the_project/first_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Topic, WebPage, AccessRecord
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'first_app/index.html')
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
+
+    return render(request, 'first_app/index.html', context=date_dict)
 
 
 def first_app(request):
